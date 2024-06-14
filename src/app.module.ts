71f7464d839b0335a4e0f10823307d4529ef5384
AMorohin <AMorohin@yandex.ru>
2024-06-14 07:46:16 +0300

    instal database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, CategoryModule, AuthModule, TransactionModule],
+  imports: [
+    UserModule,
+    CategoryModule,
+    AuthModule,
+    TransactionModule,
+    ConfigModule.forRoot({ isGlobal: true }), //модуль для доступа к переменным окружения, установлен глобально для доступа из любого модуля приложения
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
