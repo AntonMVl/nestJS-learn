431fe00153bd61843a5b7ee27c1da8a3bb0a19cb
AntonMVl <AMorohin@yandex.ru>
2024-06-14 16:28:53 +0300

    get entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,13 +10,32 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    //Подключение модулей приложения
     UserModule,
     CategoryModule,
     AuthModule,
     TransactionModule,
-    ConfigModule.forRoot({ isGlobal: true }), //модуль для доступа к переменным окружения, установлен глобально для доступа из любого модуля приложения
+    //Модуль загружает переменные окружения из файла конфигурации (например, .env). Параметр isGlobal: true указывает, что ConfigModule будет доступен глобально, и его не нужно импортировать в каждом модуле отдельно.
+    ConfigModule.forRoot({ isGlobal: true }),
+    //Блок кода настраивает подключение к базе данных с использованием TypeOrmModule. Настройка выполняется асинхронно с использованием метода forRootAsync, который позволяет загружать параметры конфигурации динамически
     TypeOrmModule.forRootAsync({
+      // imports: [ConfigModule] обеспечивает доступ к ConfigService для получения значений переменных окружения
       imports: [ConfigModule],
+      //Функция useFactory получает ConfigService через инъекцию зависимостей и использует его для получения параметров подключения к базе данных из переменных окружения (DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME). Настройки подключения включают тип базы данных (type: 'postgres'), хост, порт, имя пользователя, пароль и имя базы данных.
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        //synchronize: true - параметр указывает TypeORM автоматически синхронизировать схему базы данных с моделями. Это полезно для разработки
+        synchronize: true,
+        // entities указывает путь к файлам сущностей, которые используются в приложении
+        entities: [__dirname + '/**/*.entity{.js, .ts}'],
+      }),
+      // inject: [ConfigService] обеспечивает инъекцию ConfigService в фабричную функцию для доступа к переменным окружения
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,30 @@
-export class User {}
+import { Category } from 'src/category/entities/category.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @OneToMany(() => Category, (category) => category.user)
+  categoies: Category[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
