d3b9f530da60e95760c515949eed34a2a993b0a2
AntonMVl <AMorohin@yandex.ru>
2024-06-20 12:25:22 +0300

    categories

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,19 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadGatewayException,
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>, // Внедрение репозитория Category через конструктор
+  ) {}
+  async create(createCategoryDto: CreateCategoryDto, id: number) {
+    const isExist = await this.categoryRepository.findBy({
+      user: { id },
+      title: createCategoryDto.title,
+    });
+
+    if (isExist.length)
+      throw new BadGatewayException('This category is already exist');
+
+    const newCategory = {
+      title: createCategoryDto.title,
+      user: { id },
+    };
+
+    return await this.categoryRepository.save(newCategory);
   }
 
-  findAll() {
-    return `This action returns all category`;
+  async findAll(id: number) {
+    const userCategories = await this.categoryRepository.find({
+      where: {
+        user: { id },
+      },
+      relations: {
+        transactions: true,
+      },
+    });
+
+    if (!userCategories) throw new BadRequestException('');
+
+    return userCategories;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: number) {
+    const category = await this.categoryRepository.findOne({
+      where: { id },
+      relations: {
+        user: true,
+      },
+    });
+
+    if (!category) throw new NotFoundException('Category is not found');
+
+    return category;
   }
 
   update(id: number, updateCategoryDto: UpdateCategoryDto) {
